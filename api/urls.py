f253d6a7f84e71d6ae2ecc7946c9be8b863b7f70
Nadia <98525884+Nadia-JSch@users.noreply.github.com>
2024-06-06 10:57:44 +0200

    create model and run migration

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+router = routers.DefaultRouter(trailing_slash=True)
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path('', views.index, name='index'),
 ]
